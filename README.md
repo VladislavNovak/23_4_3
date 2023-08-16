f94c3b92a4af798a7f86dbbdd0efb940cf66e69d
Novak <novaldix@gmail.com>
2023-08-16 08:06:16 +0300

    23_4_3 added solution

--- README.md
+++ README.md
@@ -29,3 +29,20 @@ int main() {
    return 0;
 }
 ```
+
+### <font color="tomato">Полезные ссылки</font>
+
+[Макросы METANIT.COM](https://metanit.com/c/tutorial/3.3.php)
+
+[Макросы в С и С++](https://habr.com/ru/articles/546946/)
+
+[Макросы в С++](https://codeforces.com/blog/entry/17663)
+
+[Define a 'for' loop macro in C++](https://stackoverflow.com/questions/24392000/define-a-for-loop-macro-in-c)
+
+[Макросы в C/С++](http://www.codenet.ru/progr/cpp/Macros.php#:~:text=C%2FC%2B%2B-,%D0%9C%D0%B0%D0%BA%D1%80%D0%BE%D1%81%D1%8B%20%D0%B2%20C%2F%D0%A1%2B%2B,%D0%B8%20%D0%BE%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%BC%D0%B0%D0%BA%D1%80%D0%BE%D1%81%D0%B0%2C%20%D0%BE%D1%82%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%BF%D1%80%D0%BE%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC)
+
+[Создание макрос-функции в C++](https://ru.stackoverflow.com/questions/450769/%D0%A1%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5-%D0%BC%D0%B0%D0%BA%D1%80%D0%BE%D1%81-%D1%84%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D0%B8-%D0%B2-c)
+
+[Макрос для вектора](https://www.cyberforum.ru/cpp-beginners/thread2395755.html)
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,60 @@
+#include <windows.h>
 #include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+// Глобальные переменные
+#define VEC_AMOUNT 10
+#define MAX_AMOUNT 20
+
+// Получает вектор (vec), итерируется по нему и на каждом цикле позволяет обращаться к итератору (it)
+#define FOREACH(it, vec) for(auto it = vec.begin(); it < vec.end(); ++it)
+// Соединяет переданные строки, позволяя вызывать, в т.ч. функции
+#define DONE(action, name) action ##_## name
+// Позволяет вызвать любую функцию с одним аргументом
+#define APPLY(func, arg1) (func(arg1))
+// Позволяет вызвать любую функцию с двумя аргументами
+#define APPLY2(func, arg1, arg2) (func(arg1, arg2))
+
+void set_list(vector<int> &list) {
+    FOREACH(it, list) {
+            cout << "Введите число для вагона " << (it - list.begin()) << ": ";
+            std::cin >> *it;
+    }
+}
+
+void print_list(const vector<int> &list, const std::string &msg) {
+    cout << msg << ": ";
+    FOREACH(it, list) cout << *it << ((it - list.begin() == list.size() - 1) ? "\n" : ",");
+}
+
+void parse_list(const vector<int> &list) {
+    int totalAmount(0);
+    vector<int> overflowItems;
+    vector<int> emptyItems;
+
+    FOREACH(it, list) {
+        totalAmount += *it;
+        if (*it >= MAX_AMOUNT) overflowItems.emplace_back(*it);
+        else if (*it == 0) emptyItems.emplace_back(*it);
+    }
+
+    if (overflowItems.empty()) cout << "Переполненных вагонов нет" << endl;
+    else APPLY2(DONE(print, list), overflowItems, "Отчет по переполненным вагонам");
+    if (emptyItems.empty()) cout << "Пустых вагонов нет" << endl;
+    else APPLY2(DONE(print, list), emptyItems, "Отчет по пустым вагонам");
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+
+    std::vector<int> wagons(VEC_AMOUNT);
+
+    APPLY(DONE(set, list), wagons);
+    APPLY2(DONE(print, list), wagons, "Результат ввода");
+    APPLY(DONE(parse, list), wagons);
 }
